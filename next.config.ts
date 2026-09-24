4e5c3bfa1dfa362db3d1e511c807a5544960e64a
samiyantannaz1 <samiyantannaz1@gmail.com>
2026-09-24 10:50:51 +0330

    Fix deployment config for Runflare and GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+
+//   output: "export",
+
+//   ...(isProduction && {
+//     basePath: "/nava-farhadi-portfolio",
+//   }),
+
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
